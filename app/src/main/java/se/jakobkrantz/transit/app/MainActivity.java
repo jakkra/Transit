6f9c5665b7c0071704c394904b9070bf969dcd35
jakkra <ijakkra@gmail.com>
2014-12-09 01:23:40 +0100

    Removed uneccesary files

--- app/src/main/java/se/jakobkrantz/transit/app/MainActivity.java
+++ app/src/main/java/se/jakobkrantz/transit/app/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.widget.ListView;
 import se.jakobkrantz.transit.app.adapters.DrawerListAdapter;
